337cfb50280f088bf77d59b31f055188f45bfc51
Ashley Williams <ashley666ashley@gmail.com>
2016-05-26 14:43:44 -0400

    add production info to knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,5 +2,14 @@ module.exports = {
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgresql://localhost:5432/dev'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgresql://localhost:5432/dev',
+    connection: {
+      user: 'root',
+      password: process.env.DATABASE_PASSWORD,
+      database: 'dev'
+    }
   }
 };
